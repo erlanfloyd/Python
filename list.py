a7e3d1332535ed3a86373f8ac5d0bb6456c2736a
erlanfloyd <erlantemirbolat@mail.ru>
2018-04-20 18:53:51 +0600

    Add the new list file.

---
+++ list.py
@@ -0,0 +1,11 @@
+names = ['john', 'rachel', 'kano']
+colors = ['red', 'blue', 'green', 'yellow']
+for name, color in zip(names, colors):
+  print(name, '-->', color)
+  
+  
+  
+players = ['zizu', 'henry', 'ronni', 'david']
+for player in reversed(players):
+  print(player, end='')
+  
