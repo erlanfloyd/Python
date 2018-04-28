95f68ba01df19c80cdfc0af24f4a0b11f1e6f678
erlanfloyd <erlantemirbolat@mail.ru>
2018-04-28 13:51:47 +0600

    Add new file about regular expressions.

---
+++ regular_expressions_5.py
@@ -0,0 +1,15 @@
+                      #----------------- Metacharacters ---------------#
+
+import re
+
+pattern = r"gr.y"
+
+if re.match(pattern, "grey"):
+    print("Match1")
+
+if re.match(pattern, "gray"):
+    print("Match2")
+
+if re.match(pattern, "blue"):
+    print("Match3")
+                                  
