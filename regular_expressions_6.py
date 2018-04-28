dd132bce7694eac8ea1956921d407941bf18ccfa
erlanfloyd <erlantemirbolat@mail.ru>
2018-04-28 13:54:10 +0600

    Add file about regular expressions.

---
+++ regular_expressions_6.py
@@ -0,0 +1,14 @@
+import re
+
+pattern = r"^gr.y$"
+
+if re.match(pattern, "grey"):
+	print("Match 1")
+
+if re.match(pattern, "gray"):
+    print("Match 2")
+
+if re.match(pattern, "stringray"):
+    print("Match 3")
+
+        	
