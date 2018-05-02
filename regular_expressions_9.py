715923980b9102195ff891787ff0fbcb0dc51b5e
erlanfloyd <erlantemirbolat@mail.ru>
2018-05-02 17:59:44 +0600

    Add regular_expressions_9

---
+++ regular_expressions_9.py
@@ -0,0 +1,14 @@
+
+import re
+
+pattern = r"[^A-Z]"
+
+if re.search(pattern, "this is all quiet"):
+	print("Match 1")
+
+if re.search(pattern, "AbCdEfG123"):
+    print("Match 2")	
+
+if re.search(pattern, "THISISALLSHOUTING"):
+    print("Match 3")
+        
