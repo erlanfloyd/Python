9d1ae1302404797417f0823382bad7b68b28fda4
erlanfloyd <erlantemirbolat@mail.ru>
2018-05-03 19:07:15 +0600

    Add regular expressions.

---
+++ regular_expressions_13.py
@@ -0,0 +1,17 @@
+                                 #---------------------- Metacharacters -------------------------#
+import re
+
+pattern = r"ice(-)?cream"
+
+if re.match(pattern, "ice-cream"):
+    print("Match 1")
+
+if re.match(pattern, "icecream"):
+    print("Match 2")
+
+if re.match(pattern, "sausages"):
+    print("Match 3")
+
+if re.match(pattern, "ice-ice"):
+    print("Match 4")
+                                                 
