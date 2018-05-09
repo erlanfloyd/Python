844ff529fbee4a29057491d9e7c1df0f8bbac64c
Yerlan <erlantemirbolat@mail.ru>
2018-05-09 23:43:56 +0600

    Add regular expressions17.py

---
+++ regular_expressions17.py
@@ -0,0 +1,10 @@
+                #---------------- Groups -----------------#
+
+import re
+
+pattern = r"(?P<first>abc)(?:def)(ghi)"
+
+match = re.match(pattern, "abcdefghi")
+if match:
+    print(match.group("first"))
+    print(match.groups())
