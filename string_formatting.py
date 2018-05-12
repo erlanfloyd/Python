4e1b434042cad5a936ac89abb6d6007c2049ab18
Yerlan <erlantemirbolat@mail.ru>
2018-05-13 00:10:46 +0600

    Add string_formatting.py

---
+++ string_formatting.py
@@ -0,0 +1,13 @@
+
+t = [4,5,6]
+p = 'Numbers: {0} {1} {2}'.format(t[0], t[1], t[2])
+print(p)
+
+
+a = '{x},{y}'.format(x=5,y=12)
+print(a)
+
+
+str = '{c},{b},{a}'.format(a=5,b=9,c=7)
+print(str)
+
