4693bdeddc1a29ac3ae44ff8a14a79e4e4836ffb
erlanfloyd <erlantemirbolat@mail.ru>
2018-05-23 12:07:50 +0600

    Add default_values.py

---
+++ default_values.py
@@ -0,0 +1,7 @@
+                    #------------------- Default Values ---------------#
+def function(x, y, food="spam"):
+    print(food)
+
+function(1, 2)
+function(3, 4, "egg")
+                        
