06ba79eb5aa82bd78fae8fef276ca91ce9c04052
erlanfloyd <erlantemirbolat@mail.ru>
2018-06-05 13:54:25 +0600

    Add inside_functions.py

---
+++ inside_functions.py
@@ -0,0 +1,33 @@
+                                   #------------------------ Вложенные функции ---------------------#
+
+def Outside():
+	def Inside():
+		print("Canelo")
+	return Inside()
+	
+myFunc = Outside
+myFunc()
+
+
+
+
+def outside():
+	def inside():
+		print("GGG")		
+	return inside
+	
+myFunc = outside()
+myFunc()
+
+
+
+
+
+def outside():
+	x = 5
+	def inside():
+		print(x)
+	return inside 
+	
+myFunc = outside()
+myFunc()
