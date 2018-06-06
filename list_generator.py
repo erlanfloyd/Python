f200a0b1ea4bee649eadfca725a7ac22401dfca6
erlanfloyd <erlantemirbolat@mail.ru>
2018-06-06 12:01:39 +0600

    Add list_generator.py

---
+++ list_generator.py
@@ -0,0 +1,21 @@
+
+def one():
+	l = []
+	for i in range(10**4):
+		if i % 2 == 0:
+			l.append(i)
+	return l
+	
+l1 = one()
+print(l1)
+
+
+
+                         #---------------------------- Same thing --------------------#
+
+def two():
+	l = [x for x in range(10**4) if x % 2 == 0]
+	return l
+
+l2 = two()
+print(l2)
